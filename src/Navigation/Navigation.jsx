e9e6dd928456f99713e9bd0bb0c7c692242bedc0
LilHbt <lilianhumblot@gmail.com>
2024-04-02 18:52:13 +0200

    gh-page deploy

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -6,18 +6,20 @@ import ErrorPage from "../Page/ErrorPage/ErrorPage";
 import Home from "../Page/Home/Home";
 import Projects from "../Page/Projects/Projects";
 
-const Navigation= ()=>{
-    return (
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} /> 
-            <Route path="/*" element={<ErrorPage />} />
-          </Routes>
-          <Footer />
-        </Router>
-      );
-}
-export default Navigation
+const Navigation = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        {["/", "/LilianHumblot-Dev"].map((path) => (
+          <Route key={path} path={path} element={<Home />} />
+        ))}
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+export default Navigation;
